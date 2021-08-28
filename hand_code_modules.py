12799b9190c1ec586b4ce39b3ecd9c690a08020d
Christopher Chen <chris.chen23@utexas.edu>
2021-08-28 16:52:31 -0500

    Added scroll gesture

--- hand_code_modules.py
+++ hand_code_modules.py
@@ -5,6 +5,13 @@ import hand_code as htm
 import numpy as np
 import pyautogui
 
+PALM = 0
+THUMB_TIP = 4
+INDEX_FINGER_TIP = 8
+MIDDLE_FINGER_TIP = 12
+RING_FINGER_TIP = 16
+PINKY_TIP = 20
+
 def left_click(lmList):
     if len(lmList) != 0:
         shark_sign = True
@@ -45,6 +52,46 @@ def right_click(lmList):
             pyautogui.click(button='right')
             time.sleep(0.25)
 
+def scroll(lmList):
+    '''
+    find distance of pinky(location 20) and ring finger(location 16) to palm (location 0)
+    then, find distance of index finger(location 8) and middle finger(location 12) to palm (location 0)
+    4 conditions:
+        -index finger has to be triple distance of pinky and ring finger to palm
+        -ring finger has to be triple distance of pinky and ring finger to palm
+
+    once four conditions are met, initiate scroll
+    '''
+    if len(lmList) != 0:
+        MIN_DISTANCE_FACTOR = 3
+        #palm coordinates
+        palm_x = lmList[PALM][1]
+        palm_y = lmList[PALM][2]
+        #index finger coordinates
+        index_x = lmList[INDEX_FINGER_TIP][1]
+        index_y = lmList[INDEX_FINGER_TIP][2]
+        #middle finger coordinates
+        middle_x = lmList[MIDDLE_FINGER_TIP][1]
+        middle_y = lmList[MIDDLE_FINGER_TIP][2]
+        #ring finger coordinates
+        ring_x = lmList[RING_FINGER_TIP][1]
+        ring_y = lmList[RING_FINGER_TIP][2]
+        #pinky finger coordinates
+        pinky_x = lmList[PINKY_TIP][1]
+        pinky_y = lmList[PINKY_TIP][2]
+        #distances
+        distance_index = np.hypot(index_x - palm_x, index_y - palm_y)
+        distance_middle = np.hypot(middle_x - palm_x, middle_y - palm_y)
+        distance_ring = np.hypot(ring_x - palm_x, ring_y - palm_y)
+        distance_pinky = np.hypot(pinky_x - palm_x, pinky_y - palm_y)
+
+        #checking conditions
+        if distance_index > MIN_DISTANCE_FACTOR * distance_ring and distance_index > MIN_DISTANCE_FACTOR * distance_pinky and distance_middle > MIN_DISTANCE_FACTOR * distance_ring and distance_middle > MIN_DISTANCE_FACTOR * distance_pinky:
+            #initiate scroll
+            pyautogui.click(button='middle')
+            time.sleep(4)
+
+
 
 pTime = 0
 cTime = 0
@@ -53,19 +100,20 @@ cap = cv2.VideoCapture(0)
 detector = htm.handDetection(detectionCon=0.7)
 while True:
     success, img = cap.read()
+    img = cv2.flip(img, 1)
     img = detector.findHands(img)
     lmListRight = detector.findRightPos(img)
     lmListLeft = detector.findLeftPos(img)
 
     left_click(lmListLeft)
     right_click(lmListRight)
-
+    scroll(lmListRight)
+    
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
 
     cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
                 (255, 0, 255), 3)
-
     cv2.imshow("Image", img)
     cv2.waitKey(1)
